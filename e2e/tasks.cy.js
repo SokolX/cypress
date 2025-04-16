85ed0d28106ed1a57c627680b923d8c4c2fbd620
sokolx <89soek@gmail.com>
2025-04-16 21:08:07 +0200

    added tests tasks.cy.js

--- e2e/tasks.cy.js
+++ e2e/tasks.cy.js
@@ -1,9 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('tasks managment', () => {
-    it('should open and close the new task modal', () => {
+    it('should open and close the new task modal by click background', () => {
       cy.visit('http://localhost:5173');
       cy.contains('Add Task').click();
       cy.get('.backdrop').click({ force: true });
+      cy.get('.backdrop').should('not.exist');
+      cy.get('.modal').should('not.exist');
+    });
+
+    it('should open and close the new task modal by click button Cancel', () => {
+      cy.visit('http://localhost:5173');
+      cy.contains('Add Task').click();
+      cy.get('.modal').should('exist');
+      cy.contains('Cancel').click();
+      cy.get('.backdrop').should('not.exist');
+      cy.get('.modal').should('not.exist');
     });
 });
