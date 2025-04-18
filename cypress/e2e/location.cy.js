04fc7c8545018fc655923879b5f1fab0497371a7
sokolx <89soek@gmail.com>
2025-04-18 19:17:47 +0200

    use spy and clock()n tick() in location.cy.js

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="Cypress" />
 
 beforeEach(() => {
+    cy.clock();
+
     cy.fixture('user-location.json').as('userLocation');
 
     cy.visit('/').then((win) => {
@@ -18,6 +20,9 @@ beforeEach(() => {
         cy.stub(win.navigator.clipboard, 'writeText')
             .as('saveToClipboard')
             .resolves();
+            
+        cy.spy(win.localStorage, 'setItem').as('storeLocation');
+        cy.spy(win.localStorage, 'getItem').as('getStoredLocation');
     });
 }); 
 
@@ -41,7 +46,20 @@ describe('share location', () => {
                 'have.been.calledWithMatch', 
                 new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`)
             );
+
+            cy.get('@storeLocation').should(
+                'have.been.calledWithMatch', 
+                /John Doe/, 
+                new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`)
+            );
         });
-    
+        cy.get('@storeLocation').should('have.been.called');
+        cy.getById('share-loc-btn').click();
+        cy.get('@getStoredLocation').should('have.been.called');
+        cy.getById('info-message')
+            .should('be.visible')
+            .and('have.class', 'visible');
+        cy.tick(2000);
+        cy.getById('info-message').should('not.be.visible')
     });
 });
